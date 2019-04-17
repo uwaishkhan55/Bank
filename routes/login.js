1ef691ed87f86f9346e3b1b2d8e091a8aff5ca98
uwaish55 <uwaishkhan55@gmail.com>
2019-04-17 19:49:42 +0530

    remove testinomials

--- routes/login.js
+++ routes/login.js
@@ -2,6 +2,7 @@ const route = require('express').Router()
 const passport = require('passport')
 route.get('/',(req,res)=>
 {
+  if(req.user) return res.redirect('/profile')
     res.render('login')
 }
 )
